daff17dce2676527a4ff003b370964910774b9fb
snotrman <helikopterelidojosa@gmail.com>
2025-05-28 23:18:04 +0300

    changed tina build location

--- tina/config.ts
+++ tina/config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   token: process.env.TINA_TOKEN!,
   build: {
     outputFolder: "admin",
-    publicFolder: "static",
+    publicFolder: "public",
   },
   media: {
     tina: {
